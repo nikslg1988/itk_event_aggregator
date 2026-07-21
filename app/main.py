e21c99f6f665a253d50c530e0379d778c1c1b9ca
Nikita Samoylov <barmagloth1976@gmail.com>
2026-07-21 08:09:41 +0300

    fix: healthcheck

--- app/main.py
+++ app/main.py
@@ -8,6 +8,6 @@ from app.api.v1.event import router as event_router
 app = FastAPI(title=settings.app_name, version="0.1.0")
 app.include_router(event_router)
 
-@app.get("/api/health/")
+@app.get("/api/health")
 async def health_check():
     return {"status": "ok"}
