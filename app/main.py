bc682056cce4d299b855bb08806ca674161c723a
Nikita Samoylov <barmagloth1976@gmail.com>
2026-07-22 11:52:30 +0300

    feat: add ticket registration endpoints

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from app.api.v1.event import router as event_router
+from app.api.v1.tickets import router as ticket_router
 
 app = FastAPI(title="itk_event_agreggator", version="0.1.0")
 app.include_router(event_router)
